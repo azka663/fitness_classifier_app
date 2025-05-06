038b8dcd5962515a08078aa9ee65474e34ef0f17
sadia663 <sadiamalik6782@gmail.com>
2025-05-06 20:01:34 +0500

    Update Aapp.py

--- Aapp.py
+++ Aapp.py
@@ -1,3 +1,7 @@
+
+
+
+
 import streamlit as st
 import numpy as np
 from tensorflow.keras.models import load_model
@@ -11,12 +15,14 @@ if not os.path.exists(model_path):
     # Corrected URL format for gdown
     url = "https://drive.google.com/uc?id=1KLcdAgZ7lUqI0HZpl89FbeBtUU-VWAsr"
     gdown.download(url, model_path, quiet=False)
-
-# Load the model
+    
+from tensorflow.keras.models import load_model
 model = load_model(model_path)
+# Load the model
+model = load_model("fitness_model_3class.h5")
 
 # Define class labels
-labels = ["Fit", "Overweight", "Underweight"]
+labels = ["Fit ", "Overweight ", "Underweight "]
 
 # Page title
 st.set_page_config(page_title="Body Fitness Image Classifier", layout="centered")
@@ -30,7 +36,7 @@ uploaded_file = st.file_uploader("📁 Upload an image", type=["jpg", "jpeg", "p
 if uploaded_file is not None:
     # Display uploaded image
     image = Image.open(uploaded_file).convert("RGB")
-    st.image(image, caption="Uploaded Image", use_column_width=True)
+    st.image(image, caption=" Uploaded Image", use_column_width=True)
 
     # Preprocess the image
     img = image.resize((150, 150))
@@ -45,4 +51,4 @@ if uploaded_file is not None:
     # Display result
     st.markdown("---")
     st.markdown(f"<h3 style='text-align: center;'>Prediction: <span style='color:#4CAF50;'>{labels[predicted_index]}</span></h3>", unsafe_allow_html=True)
-    st.markdown(f"<h4 style='text-align: center;'>Confidence Score: {confidence:.2f}</h4>", unsafe_allow_html=True)
+    st.markdown(f"<h4 style='text-align: center;'> Confidence Score: {confidence:.2f}</h4>", unsafe_allow_html=True)
